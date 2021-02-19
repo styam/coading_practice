fed550c3035419d3ee7f216b592f87ebefa574a2
satyam <satyam.hsmishra@gmail.com>
2021-02-19 16:47:01 +0530

    added new dp

---
+++ interview/count_min_subset.py
@@ -0,0 +1,26 @@
+# Python program to find number of subset containing 
+# consecutive numbers 
+def numofsubset(arr, n): 
+
+# Sort the array so that elements which are consecutive 
+# in nature became consecutive in the array. 
+x = sorted(arr) 
+
+count = 1
+
+for i in range(0, n-1): 
+
+	# Check if there is beginning of another subset of 
+	# consecutive number 
+	if (x[i] + 1 != x[i + 1]): 
+	count = count + 1
+
+return count 
+
+# Driven Program 
+arr = [ 100, 56, 5, 6, 102, 58, 101, 57, 7, 103, 59 ] 
+n = len(arr) 
+print numofsubset(arr, n) 
+
+# This code is contributed by Afzal Ansari. 
+
